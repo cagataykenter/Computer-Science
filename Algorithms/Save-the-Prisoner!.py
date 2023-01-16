b43074b9a72d4ffd987367aa424bc2f7e857271b
çağatay <79807950+cagataykenter@users.noreply.github.com>
2023-01-16 21:10:26 +0300

    Update Save-the-Prisoner!.py

--- Algorithms/Save-the-Prisoner!.py
+++ Algorithms/Save-the-Prisoner!.py
@@ -1,1 +1,54 @@
 
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'saveThePrisoner' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER m
+#  3. INTEGER s
+#
+
+def saveThePrisoner(n, m, s):
+    if m > n:
+        floor = m // n
+        m -= floor * n
+    if m == 0:
+        if s > 1:
+            return s-1
+        else:
+            return n
+
+    temp = s + m - 1
+    if temp <= n:
+        return temp
+    else:
+        return temp - n   
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        first_multiple_input = input().rstrip().split()
+
+        n = int(first_multiple_input[0])
+
+        m = int(first_multiple_input[1])
+
+        s = int(first_multiple_input[2])
+
+        result = saveThePrisoner(n, m, s)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
